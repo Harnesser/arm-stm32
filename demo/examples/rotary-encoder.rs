36f24a2e1ca6f3deb45f21b77f441016f105eaae
Marty <harnesser+kartoffel@gmail.com>
2017-06-18 00:10:39 +0100

    unworking version

--- demo/examples/rotary-encoder.rs
+++ demo/examples/rotary-encoder.rs
@@ -47,7 +47,6 @@ extern crate stm32f100;
 use core::u16;
 use cast::{u16,u32};
 
-use cortex_m::asm;
 use stm32f100::{GPIOA, RCC, TIM7, NVIC};
 use stm32f100::interrupt;
 use stm32f100::interrupt::{Interrupt};
@@ -60,8 +59,11 @@ mod frequency {
 /// Timer Frequency
 const FREQUENCY: u32 = 100; // Hz
 
-//static mut counter: i32 = 0;
-//static mut call_counter: i32 = 0;
+#[allow(non_upper_case_globals)]
+static mut counter: i32 = 0;
+
+#[allow(non_upper_case_globals)]
+static mut call_counter: i32 = 0;
 
 #[inline(never)]
 fn main() {
@@ -127,7 +129,55 @@ fn main() {
         cortex_m::interrupt::enable();
     }
 
+    let mut cnt = 0;
+    loop {
+        if cnt == 8_000_000 {
+            cnt = 0;
+        } else {
+            cnt += 1;
+        }
+        if cnt == 0 {
+            unsafe {
+                hprintln!("{} {}", counter, call_counter);
+            }
+        }
+    }
+
 }
+
+mod rotary {
+
+    #[derive(Copy, Clone, PartialEq)]
+    pub enum EncoderState {
+        Idle,
+        CW01,
+        CW00,
+        CW10,
+        CCW10,
+        CCW00,
+        CCW01,
+    }
+
+    pub static LUT : [[EncoderState; 4]; 7] = [
+        //                  00                    01                   10               11
+        /* Idle  */ [EncoderState::Idle,  EncoderState::CW01,  EncoderState::CCW10, EncoderState::Idle],
+        /* CW01  */ [EncoderState::CW00,  EncoderState::CW01,  EncoderState::CW01,  EncoderState::Idle],
+        /* CW00  */ [EncoderState::CW00,  EncoderState::CW01,  EncoderState::CW10,  EncoderState::Idle],
+        /* CW10  */ [EncoderState::CW00,  EncoderState::CW10,  EncoderState::CW10,  EncoderState::Idle],
+        /* CCW10 */ [EncoderState::CCW00, EncoderState::CCW10, EncoderState::CCW10, EncoderState::Idle],
+        /* CCW00 */ [EncoderState::CCW00, EncoderState::CCW01, EncoderState::CCW10, EncoderState::Idle],
+        /* CCW01 */ [EncoderState::CCW00, EncoderState::CCW01, EncoderState::CCW01, EncoderState::Idle],
+    ];
+
+}
+
+
+use rotary::EncoderState;
+
+#[allow(non_upper_case_globals)]
+static mut state_enc01: EncoderState = EncoderState::Idle;
+
+static GPIOA_MASK : u32 = 5;
 
 extern "C" fn rotary_encoder_handler(_ctxt: interrupt::Tim7Irq) {
 
@@ -137,6 +187,25 @@ extern "C" fn rotary_encoder_handler(_ctxt: interrupt::Tim7Irq) {
         |cs| {
             let tim7 = TIM7.borrow(cs);
             tim7.sr.modify(|_,w| w.uif().clear());
+
+            // read the pins
+            let gpioa = GPIOA.borrow(cs);
+            let value = gpioa.idr.read().bits() & GPIOA_MASK;
+            let input = (value >> 1) | (value & 1);
+
+            unsafe {
+                call_counter += 1;
+                let next_state = rotary::LUT[state_enc01 as usize][input as usize];
+
+                if (state_enc01 == EncoderState::CW10) && (next_state == EncoderState::Idle) {
+                    counter -= 1;
+                } else if (state_enc01 == EncoderState::CCW01) && ( next_state == EncoderState::Idle) {
+                    counter += 1;
+                }
+
+                state_enc01 = next_state;
+            }
+
         }
     );
 
